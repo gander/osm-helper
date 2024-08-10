4618723a89291aefad61a8845e0e64c422af56cd
Adam Gąsowski <gander@users.noreply.github.com>
2024-08-10 10:41:38 +0200

    feat: improve extractAndValidateId

--- src/common.ts
+++ src/common.ts
@@ -1,9 +1,9 @@
 export type ExtractedId = number | null;
 
-export const extractAndValidateId = (path: string, matcher: RegExp): ExtractedId => {
+export const extractAndValidateId = (path: string, matcher: RegExp, matchPos: number = 1): ExtractedId => {
     const match = path.match(matcher);
     if (match) {
-        const id = parseInt(match[1], 10);
+        const id = parseInt(match[matchPos], 10);
         return isNaN(id) ? null : id;
     }
     return null;
